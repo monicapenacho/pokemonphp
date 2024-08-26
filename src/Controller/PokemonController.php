d65452a54eb5de6b5a74375e3cbf0a83fe8978bc
Chiara Magnani <chiara2mf@gmail.com>
2024-08-26 09:45:41 +0200

    mostrar listado de pokemons

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -19,4 +19,37 @@ class PokemonController extends AbstractController
 
         return $this->render("pokemons/getpokemon.html.twig", ["pokemon" => $pokemon]);
     }
+
+    #[Route("/allpokemon")]
+    public function getAllPokemon()    //Devuleve resultado de renderizar plantilla twig
+    {
+        $allpokemon = [
+            [
+                "nombre" => "Emolga",
+                "descripcion" => "Planea por el aire, casi como si danzara, mientras desprende electricidad. Resulta adorable, pero puede causar bastantes problemas.",
+                "imagen" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/587.png",
+                "codigo" => 587,
+            ],
+            [
+                "nombre" => "Pikachu",
+                "descripcion" => "Planea por el aire, casi como si danzara, mientras desprende electricidad. Resulta adorable, pero puede causar bastantes problemas.",
+                "imagen" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/125.png",
+                "codigo" => 587,
+            ],
+            [
+                "nombre" => "Bulbasur",
+                "descripcion" => "Planea por el aire, casi como si danzara, mientras desprende electricidad. Resulta adorable, pero puede causar bastantes problemas.",
+                "imagen" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/345.png",
+                "codigo" => 587,
+            ],
+            [
+                "nombre" => "Charmander",
+                "descripcion" => "Planea por el aire, casi como si danzara, mientras desprende electricidad. Resulta adorable, pero puede causar bastantes problemas.",
+                "imagen" => "https://www.pokemon.com/static-assets/content-assets/cms2/img/pokedex/full/666.png",
+                "codigo" => 587,
+            ]
+        ];
+
+        return $this->render("pokemons/getallpokemon.html.twig", ["allpokemon" => $allpokemon]);
+    }
 }
